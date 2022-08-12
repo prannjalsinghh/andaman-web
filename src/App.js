847bf02f9f86ca59a1b0913e4ed5fc24661a67a1
khushijain3103 <khushijain312003@gmail.com>
2022-08-12 15:01:16 +0530

    hehe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Footer from './Components/Footer';
-import SearchBar from './Components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <SearchBar/>
       <Footer/>
     </div>
   );
