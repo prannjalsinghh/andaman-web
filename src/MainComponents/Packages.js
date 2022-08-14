456f6c9cddeb4b6833778cc03bae79e569ad561b
khushijain3103 <khushijain312003@gmail.com>
2022-08-14 17:52:30 +0530

    added packages page

--- src/MainComponents/Packages.js
+++ src/MainComponents/Packages.js
@@ -1,12 +1,25 @@
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import Banner from "../Components/Banner";
+import Services from "../Components/Services";
 
-const Packages = ()=>{
+const Packages = (props)=>{
     return(
         <>
             <Header/>
             <Banner url="https://img.kantipurholidays.com/ContentImages/IMG__82bacd2d-2094-42e7-819e-5860405b81af-0-1941x1132.jpeg"/>
+            <div>
+                <h2>{props.items.heading}</h2>
+                <p>{props.items.info}</p>
+                <p>{props.items.place}<span>{props.items.time}</span></p>
+                <p>{props.items.number}<span>{props.items.day}</span></p>
+                <p>{`(${props.items.TourCode})`}</p>
+            </div>
+            <Services  item={{heading:"HIGHLIGHTS"}}/>
+            <Services item={{heading:"ITERNARY OVERVIEW" , info:"2 Nights Port Blair – 2 Night Havelock Island"}}/>
+            <Services item={{heading:"ITERNARY IN DETAIL"}}/>
+            <Services item={{heading:"ACCOMMODATION USED"}}/>
+            <Services item={{heading:"Ferry Timing"}}/>
             <Footer/>
         </>
     )
