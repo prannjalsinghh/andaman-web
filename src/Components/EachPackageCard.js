882a469be76bb67f9daa80beb8aa1d7d9117f454
Pranjal <pranjalsingh531@gmail.com>
2022-08-15 00:54:03 +0530

    partial

--- src/Components/EachPackageCard.js
+++ src/Components/EachPackageCard.js
@@ -12,13 +12,13 @@ const EachPackageCard = (props)=>{
 
     return(
 
-        <div className='p-7 w-64 border border-gray-300 rounded-md text-center bg-white  hover:scale-90  transition-all gap-3'>
+        <div className='box-content	p-7 w-64 border border-gray-300 rounded-md text-center bg-white  hover:scale-90  transition-all gap-3'>
 
-            <img src={props.item.url} />
+            <img className="h-40"src={props.item.url} />
             <div className='flex flex-col items-center  justify-between gap-2'>
                 
                     <h3 className='text-2xl text-center font-semibold'>{props.item.heading}</h3>
-                    <p>{props.item.info}</p>
+                    <p className=''>{props.item.info}</p>
                     <p>{`TOURCODE-${props.item.TourCode}`}</p>
                     <button className='p-1.5 rounded-md' onClick={renderNewPackage}>View Details</button>
                 
--- src/MainComponents/Packages.js
+++ src/MainComponents/Packages.js
@@ -5,19 +5,23 @@ import Services from "../Components/Services";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import CustomizedTables from '../Components/Table'
+import EachPackageCard from "../Components/EachPackageCard";
 
 const Packages = () => {
   const location = useLocation();
   const [item, setItem] = useState([]);
+  const [mainArray,setLoadedArray] =useState([]);
   function sort() {
     setItem(items.find((x) => x.TourCode === location.state.id));
   }
   useEffect(() => {
     sort();
+    allPackages();
   }, []);
 
   const items = [
     {
+        url:"https://images.news18.com/ibnlive/uploads/2022/04/shutterstock_1064304320-16504498334x3.jpg",
       TourCode: "MTG 012",
       heading: "Amazing Neil & Havelock",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -30,6 +34,7 @@ const Packages = () => {
       nights: "5",
     },
     {
+        url:"https://static.toiimg.com/thumb/69007547/andaman.jpg?width=1200&height=900",
       TourCode: "MTG 004",
       heading: "Havelock(Swaraj Dweep) Delight",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -42,6 +47,7 @@ const Packages = () => {
       nights: "6",
     },
     {
+        url:"https://media.istockphoto.com/photos/scenic-sea-beach-at-havelock-islands-andaman-india-at-sunset-picture-id1040379102?k=20&m=1040379102&s=612x612&w=0&h=TWWIWrQsFZBMi8TEJk1qZZzqIhSgQB4gtMwHvNpTzKg=",
       TourCode: "MTG 006",
       heading: "Amazing Andaman with Baratang",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -54,6 +60,7 @@ const Packages = () => {
       nights: "5",
     },
     {
+        url:"https://img.onmanorama.com/content/dam/mm/en/travel/outside-kerala/images/2021/10/19/andaman-nicobar.jpg",
       TourCode: "MTG 002",
       heading: "Amazing Andaman with Jolly Buoy",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -66,6 +73,7 @@ const Packages = () => {
       nights: "4",
     },
     {
+        url:"https://static.toiimg.com/photo/68814397.cms",
       TourCode: "MTG 007",
       heading: "Andaman Delight with Jolly Buoy",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -78,6 +86,7 @@ const Packages = () => {
       nights: "6",
     },
     {
+        url:"https://img.traveltriangle.com/blog/wp-content/uploads/2020/01/cover_13-jan1.jpg",
       TourCode: "MTG 001",
       heading: "Andaman Delight with Baratang",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -89,6 +98,7 @@ const Packages = () => {
       nights: "3",
     },
     {
+        url:"https://images.unsplash.com/photo-1586053226626-febc8817962f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YW5kYW1hbiUyMGFuZCUyMG5pY29iYXIlMjBpc2xhbmRzJTIwaW5kaWF8ZW58MHx8MHx8&w=1000&q=80",
       TourCode: "MTG 003",
       heading: "Magical Andaman",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -100,6 +110,7 @@ const Packages = () => {
       nights: "4",
     },
     {
+        url:"https://s01.sgp1.cdn.digitaloceanspaces.com/article/157373-zcpsrpnnec-1617976605.jpg",
       TourCode: "MTG 005",
       heading: "Magical Andaman with Jolly Buoy",
       info: "A perfect program to hop in from one island to other. This trip takes you to the best-rated attraction in Andaman Islands during your short trip to the Amazing Andaman Island. Explore white sand beaches, boat rides during this trip.",
@@ -144,6 +155,24 @@ const Packages = () => {
     },
     },
   ];
+
+  const allPackages = ()=>{
+    let loadedArray = [];
+    console.log(item);
+    for(let key in items){
+        if(items[key].TourCode===item.TourCode){
+            continue;
+        }
+        else{
+            loadedArray.push(items[key])
+        }
+    }
+    setLoadedArray(loadedArray);
+  }
+  const infoHandler=()=>{
+
+  }
+
   return (
     <>
       <Header />
@@ -178,6 +207,11 @@ const Packages = () => {
 
       <Services item={{ heading: "ITERNARY IN DETAIL" }} />
       <Services item={{ heading: "ACCOMMODATION USED" }} />
+      <marquee behavior="scroll" direction="left" scrollamount="10" onmouseover="this.stop();" className="h-full" onmouseout="this.start();">
+      <div className="flex">
+        {mainArray.map((each)=><EachPackageCard item={{heading:`${each.nights}Nights - ${each.days}Days`,info:`${each.places[0]?.time} ${each.places[0]?.place} - ${each.places[1]?.time} ${each.places[1]?.place} `, TourCode:each.TourCode, url:each.url}}/>)}
+      </div>
+      </marquee>
       <Services item={{ heading: "Ferry Timing" }} />
       <Footer />
     </>
