b305b877cf800e6f66a4036d8cb419788acf41f0
khushijain3103 <khushijain312003@gmail.com>
2022-08-14 17:33:55 +0530

    added dynamic table

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -28,47 +28,19 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(
-  name,
-  Calories,
-  fat,
-  carbs,
-  protein,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function CustomizedTables() {
+export default function CustomizedTables(props) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            {props.head.map((cell) => <StyledTableCell>{cell}</StyledTableCell> )}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                 {props.col.map((cell) => <StyledTableCell>{cell}</StyledTableCell> )}
             </StyledTableRow>
           ))}
         </TableBody>
--- src/MainComponents/Packages.js
+++ src/MainComponents/Packages.js
@@ -1,16 +1,12 @@
-import EachPackageCard from "../Components/EachPackageCard";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import Banner from "../Components/Banner";
 
 const Packages = ()=>{
-    const items = [{url:'https://media.istockphoto.com/photos/family-vacation-holiday-happy-family-running-on-the-beach-in-the-picture-id1299265795?k=20&m=1299265795&s=612x612&w=0&h=kKrVYwdoNvKti2dq9xP5fnoktnLFLgwc5HMeqRixyE4=',heading:'Andaman Family Tour Package',info:"4 Nights 3 Days"},{url:'https://media.istockphoto.com/photos/family-vacation-holiday-happy-family-running-on-the-beach-in-the-picture-id1299265795?k=20&m=1299265795&s=612x612&w=0&h=kKrVYwdoNvKti2dq9xP5fnoktnLFLgwc5HMeqRixyE4=',heading:'Andaman Family Tour Package',info:"4 Nights 3 Days"}]
     return(
         <>
             <Header/>
-            <div className="flex flex-col justify-around sm:flex-row">
-            <EachPackageCard item={items[0]}/>
-            <EachPackageCard item={items[1]}/>
-            </div>
+            <Banner url="https://img.kantipurholidays.com/ContentImages/IMG__82bacd2d-2094-42e7-819e-5860405b81af-0-1941x1132.jpeg"/>
             <Footer/>
         </>
     )
