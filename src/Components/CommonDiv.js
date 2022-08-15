d7afae6a18f835be6a72af7bd89e9bfd3bfdc292
khushijain3103 <khushijain312003@gmail.com>
2022-08-15 13:46:59 +0530

    added common div css

--- src/Components/CommonDiv.js
+++ src/Components/CommonDiv.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Container } from '@mui/material';
 
 const CommonDiv = (props) => {
 
     return(
-        <div>
-            <h1 className='text-2xl font-semibold text-orange-700'> {props.title}</h1>
-            <ul>
+        <Container className='mt-10'>
+            <h1 className='text-2xl font-semibold text-orange-600'> {props.title}</h1>
+            <h3>{props.subtitle}</h3>
+            <ul className='mt-5 list-decimal font-bold text-gray-700'>
                 {props.arr.map((item) => <li>{item}</li>)}
             </ul>
-        </div>
+        </Container>
     )
 
 };
