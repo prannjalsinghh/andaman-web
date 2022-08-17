4e7531b1877d8da3b5e70abfb33c9458f01715d6
Pranjal <pranjalsingh531@gmail.com>
2022-08-18 01:13:36 +0530

    updated logo

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Divider } from "@mui/material";
 const Services = (props) => {
   return (
-    <div className={`flex flex-col items-center text-center  m-auto mt-10 gap-2 mb-5`}>
-      <h1 className={`   ${props.call==="header"?"text-3xl sm:text-4xl text-white hover:text-orange-600 font-extrabold":" text-orange-600 text-4xl font-semibold"} `}>
+    <div className={`flex flex-col ${props.call==="header"?" ": "items-center gap-2"} text-center  m-auto mt-10  mb-5`}>
+      <h1 className={`   ${props.call==="header"?"text-3xl sm:text-4xl text-white hover:text-orange-600 font-black":" text-orange-600 text-4xl font-semibold"} `} 
+      
+      >
         {props.item.heading}
       </h1>
-      <div className="flex flex-1 justify-center items-center">
+      <div className={`flex flex-1 justify-center items-center  ${props.call==="header"?"gap-2":" "}`}>
         <Divider
-          style={{ color: "orange", width: "50px", backgroundColor: "orange" }}
+          style={{ color: "", width: "30px",height:"3px", backgroundColor: "rgb(245,105,65)" }}
         />
         <div className="">{props.item.icon}</div>
         <Divider
-          style={{ color: "orange", width: "50px", backgroundColor: "orange" }}
+          style={{ color: "", width: "30px",height:"3px", backgroundColor: "rgb(245,105,65)" }}
         />
       </div>
       <div className="text-center font-bold text-gray-600">{props.item.info}</div>
